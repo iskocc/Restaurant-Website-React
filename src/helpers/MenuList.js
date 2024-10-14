08c5b50cf8d690a9a0a69e6c9b575a700527fb9d
iskocc <kocakismail87@gmail.com>
2024-10-14 18:16:53 +0300

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Beef from "../assets/1beef.jpg"
+import Blackbean from "../assets/2blackbean.jpg"
+import Chicken from "../assets/3chicken.jpg"
+import Turkey from "../assets/4turkey.jpeg"
+import Veggie from "../assets/5veggie.jpg"
+import Cheese from "../assets/6cheese.jpg"
+
+export const MenuList = [
+    {
+        name:"Beef Burger",
+        image:Beef,
+        price:300,
+    },
+    {
+        name:"BlackBean Burger",
+        image:Blackbean,
+        price:280,
+    },
+    {
+        name:"Chicken Burger",
+        image:Chicken,
+        price:225,
+    },
+    {
+        name:"Turkey Burger",
+        image:Turkey,
+        price:190,
+    },
+    {
+        name:"Veggie Burger",
+        image:Veggie,
+        price:240,
+    },
+    {
+        name:"Cheese Burger",
+        image:Cheese,
+        price:250,
+    },
+];
